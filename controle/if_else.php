99b32779808eec7933e60bffe9fb85d222482189
Alexsandro Acioli <alexacioli.08@gmail.com>
2025-08-24 22:41:28 -0300

    up

--- controle/if_else.php
+++ controle/if_else.php
@@ -7,10 +7,10 @@ $nota2 = 1;
 
 $media = ($nota1 + $nota2) / 2;
 
-if($media >= 7) {
+if($media >= 7) { // primeira verificaçõa
     echo "Aprovado com a media: $media";
-}else if($media >= 6){
+}else if($media == 6){ // segunda verificação
     echo "Recuperação com a media: $media";
-}else{
+}else{ // caso não seja senhuma das veriicações a cima
     echo "Reprovado com a media: $media";
 }
---
+++ controle/operadores_relacionados.php
@@ -0,0 +1,49 @@
+<div class="titulo">Operadores Relacionais</div>
+
+<?php
+
+var_dump(1 == 1); // True
+echo '<br>';
+var_dump(1 > 1); // False
+echo '<br>';
+var_dump(1 >= 1); // True
+echo '<br>';
+var_dump(1 < 2); // True
+echo '<br>';
+var_dump(1 <= 2); // True
+echo '<br>';
+var_dump(1 != 1); // False
+echo '<br>';
+var_dump(1 <> 1); // False
+echo '<br>';
+
+var_dump(1 == "1"); // True pois aqui ele so verifica se existe algum dado igual naquele dado, mesmo sendo de outro tipo
+echo '<br>';
+var_dump(1 === "1"); // False pois aqui compara o tipo do dado
+echo '<br>';
+var_dump(1 == "123"); // False pois so tem o numero 1 nessa sequencia, equando na comparação tem 3;
+echo '<br>';
+var_dump(123 == "123"); //True pois aqui tem todos os dados iquais uns aos outros
+echo '<br>';
+var_dump(123 != "123"); //False pois ele valida que mesmo os tipos de dados sendo diferentes, o valor delas são iguais
+echo '<br>';
+var_dump(123 !== "123"); //True pois os tipos de dados são diferentes
+echo '<br>';
+
+echo "<p>Spaceship<hr></p>";
+
+var_dump(50 <=> 30);
+var_dump(50 <=> 50);
+var_dump(50 <=> 300);
+
+?>
+
+<style>
+    p{
+        color: red;
+        margin-bottom: 0;
+    }
+    hr{
+        margin: 0;
+    }
+</style>
