539c0be369c3e2f46e96668106d04bccee605321
Alexsandro Acioli <alexacioli.08@gmail.com>
2025-11-24 16:01:47 -0300

    Começando a nova seção do curso sobre Estrutura de Controle PT2

---
+++ controle2/for.php
@@ -0,0 +1,1 @@
+<div class="titulo">Estrutura de controle For</div>
---
+++ controle2/foreach.php
@@ -0,0 +1,38 @@
+<div class="titulo">Foreach</div>
+
+<?php
+
+$array = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sabado"
+];
+
+foreach($array as $a){
+    echo $a;
+    echo "<br>";
+} 
+
+echo "<hr>";
+
+$array2 = [
+    ['a','e','i','o','u'],
+    ['b','c','d']
+];
+
+foreach($array2 as $linhas){
+    foreach($linhas as $colunas){
+        echo "$colunas ";
+    }
+    echo "<br>";
+}
+
+if ($learn) $money++; else $money--;
+
+
+
+?>
